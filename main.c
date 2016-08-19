2230162ee0528b727e4026a5f19f9f58dd13b361
dimitri <dimitri.pater@gmail.com>
2016-08-19 22:00:46 +0200

    sprites

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ void game_quit(void);
 struct {
     // define "attributes"
     SDL_bool running;
+
     struct {
         unsigned int w;
         unsigned int h;
@@ -28,6 +29,11 @@ struct {
         SDL_Window* window;
         SDL_Renderer* renderer;
     } screen;
+
+    struct {
+        unsigned int n;
+        SDL_Surface** spritesheet;
+    } gfx;
 
     // define "methods"
     void (*init)(void);
@@ -38,13 +44,12 @@ struct {
                 SCREEN_SCALE*SCREEN_w,
                 SCREEN_SCALE*SCREEN_H,
                 SCREEN_NAME,
-                NULL,
-                NULL
+                NULL, NULL
         },
+        {0, NULL},
         game_init,
         game_quit
-};
-//----------------------------------------------------------------------
+};//----------------------------------------------------------------------
 void game_init(void) {
     if(SDL_Init(SDL_INIT_EVERYTHING)!=0) {
         printf("SDL error -> %sn", SDL_GetError());
@@ -67,14 +72,43 @@ void game_init(void) {
     );
 
     Game.running = SDL_TRUE;
+
+    SDL_Surface* surface = SDL_LoadBMP("/home/dimitri/ClionProjects/untitled/spritesheet.bmp");
+    int n = ((surface->w/8)*(surface->h/8)+1);
+
+    Game.gfx.n = n;
+    Game.gfx.spritesheet = (SDL_Surface**) malloc(sizeof(SDL_Surface*)*n);
+
+    int i, x, y;
+    SDL_Rect rect = {0, 0, 8, 8};
+    for(i=0; i<n; i++) {
+        Game.gfx.spritesheet[i] = SDL_CreateRGBSurface(0, 8, 8, 24, 0x00, 0x00, 0x00, 0x00);
+        SDL_SetColorKey(Game.gfx.spritesheet[i], 1, 0xFF00FF);
+        SDL_FillRect(Game.gfx.spritesheet[i], 0, 0xFF00FF);
+        if(i!=0) {
+            x = (i-1)%(surface->w/8);
+            y = (i-x)/(surface->w/8);
+            rect.x = x*8;
+            rect.y = y*8;
+            SDL_BlitSurface(surface, &rect, Game.gfx.spritesheet[i], NULL);
+        }
+    }
+
+    SDL_FreeSurface(surface);
 }
 //----------------------------------------------------------------------
 void game_quit(void) {
-    SDL_DestroyRenderer(Game.screen.renderer);
-    SDL_DestroyWindow(Game.screen.window);
+    int i;
+    for(i=0; i<Game.gfx.n; i++)
+        SDL_FreeSurface(Game.gfx.spritesheet[i]);
+    free(Game.gfx.spritesheet);
+    Game.gfx.spritesheet = NULL;
 
-    Game.screen.window = NULL;
+    SDL_DestroyRenderer(Game.screen.renderer);
     Game.screen.renderer = NULL;
+
+    SDL_DestroyWindow(Game.screen.window);
+    Game.screen.window = NULL;
 
     SDL_Quit();
 }
@@ -83,6 +117,14 @@ void game_quit(void) {
 int main(int argc, char* argv[]) {
 
     Game.init();
+
+    int x = Game.screen.w/2-8, y = Game.screen.h/2-8;
+
+    SDL_Rect rect = {0, 0, 8*2, 8*2};
+    SDL_Texture* texture1 = SDL_CreateTextureFromSurface(Game.screen.renderer, Game.gfx.spritesheet[17]);
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(Game.screen.renderer, Game.gfx.spritesheet[18]);
+    SDL_Texture* texture3 = SDL_CreateTextureFromSurface(Game.screen.renderer, Game.gfx.spritesheet[29]);
+    SDL_Texture* texture4 = SDL_CreateTextureFromSurface(Game.screen.renderer, Game.gfx.spritesheet[30]);
 
     SDL_Event event;
     while(Game.running) {
@@ -95,8 +137,26 @@ int main(int argc, char* argv[]) {
         }
 
         SDL_RenderClear(Game.screen.renderer);
+
+        rect.x = 0+x, rect.y = 0+y;
+        SDL_RenderCopy(Game.screen.renderer, texture1, NULL, &rect);
+
+        rect.x = 8*2+x, rect.y = 0+y;
+        SDL_RenderCopy(Game.screen.renderer, texture2, NULL, &rect);
+
+        rect.x = 0+x, rect.y = 8*2+y;
+        SDL_RenderCopy(Game.screen.renderer, texture3, NULL, &rect);
+
+        rect.x = 8*2+x, rect.y = 8*2+y;
+        SDL_RenderCopy(Game.screen.renderer, texture4, NULL, &rect);
+
         SDL_RenderPresent(Game.screen.renderer);
     }
+
+    SDL_DestroyTexture(texture1);
+    SDL_DestroyTexture(texture2);
+    SDL_DestroyTexture(texture3);
+    SDL_DestroyTexture(texture4);
 
     Game.quit();
 
